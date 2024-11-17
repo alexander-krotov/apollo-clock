fa0b5bb0d90b1e1785d810049637ff4b2f209236
Alexander Krotov <alexander.krotov@gmail.com>
2024-11-17 20:05:40 +0200

    WebUI implemented.
    RTC access was broken somewhere.

--- apollo-clock.ino
+++ apollo-clock.ino
@@ -12,6 +12,7 @@
 #include <TimeLib.h>
 #include <WiFiUdp.h>
 #include <DS3231.h>
+#include <GyverPortal.h>
 
 // 74HC595 control pins
 #define SER_PIN 4
@@ -22,18 +23,24 @@
 // ESP32 onboard LED pin
 #define LED_PIN 8
 
-// Maximum brightnes level
-const int MAX_BRIGHTNES=16;
+// Maximum brightness level
+const int MAX_BRIGHTNESS=16;
 
 // Display blink time in milliseconds.
 const int BLINK_TIME=50;
 
-// Clodk global configuration.
+// NTP update interval in seconds
+const int NTP_UPDATE_INTERVAL=3000;
+
+// Clock global configuration.
 char ntpServerName[80] = "fi.pool.ntp.org";
 signed char clock_tz = 2; // Timezone shift (could be negative)
-unsigned char clock_brightness = MAX_BRIGHTNES; // Clock brightnes (1-MAX_BRIGHTNES).
+unsigned char clock_brightness = MAX_BRIGHTNESS; // Clock brightness (1-MAX_BRIGHTNESS).
 unsigned char clock_12 = 0;  // If non-zero clock is 12h, otherwise 24h
 unsigned char clock_leading_0;  // Show hour leading 0
+unsigned char clock_bar_mode = 0;   // bar mode
+unsigned char clock_use_ntp = true;  // Use NTP switch
+unsigned char clock_use_rtc = true;  // Use RTC switch
 
 // Clock eeprom data address.
 const int eeprom_addr=12;
@@ -41,99 +48,21 @@ const int eeprom_addr=12;
 // Real time clock
 DS3231 myRTC;
 
+// Web interface
+GyverPortal ui;
+
 // Initialize the network, and update the global configuration if
 // needed.
 bool initialize_network()
 {
-  char timezone_shift[20];
-  char brightness_str[20];
-  char n12_str[3]="24";
-  char leading0[3]="n";
-
-  if (clock_12) {
-    strcpy(n12_str, "12");
-  }
-
-  if (clock_leading_0) {
-    strcpy(leading0, "y");
-  }
-
-  itoa(clock_brightness, brightness_str, 10);
-  itoa(clock_tz, timezone_shift, 10);
-
   WiFiManager wm;
   // wm.resetSettings();
-
-  WiFiManagerParameter tz("timezone_shift", "Timezone shift", timezone_shift, 8);
-  WiFiManagerParameter brightness("brightness", "Brightness", brightness_str, 8);
-  WiFiManagerParameter ntp_server("ntp_server", "NTP Server", ntpServerName, sizeof(ntpServerName)-1);
-  WiFiManagerParameter n12("n12", "12/24", n12_str, sizeof(n12_str)-1);
-  WiFiManagerParameter leading_zero("leading0", "Leading zero (y/n)", leading0, sizeof(leading0)-1);
-
-  wm.addParameter(&tz);
-  wm.addParameter(&brightness);
-  wm.addParameter(&ntp_server);
-  wm.addParameter(&n12);
-  wm.addParameter(&leading_zero);
 
   // Automatically connect using saved credentials,
   // if connection fails, it starts an access point with the name "NixieClock".
   wm.setConfigPortalTimeout(60);
-  bool res = wm.autoConnect("NixieClock"); // anonymous ap
-
-  const char *str_val = tz.getValue();
-  if (str_val) {
-    int v = atoi(str_val);
-    if (v>=-12 && v<=12) {
-      if (clock_tz != v) {
-        clock_tz = v;
-        EEPROM.write(eeprom_addr, clock_tz);
-        EEPROM.commit();
-      }
-    }
-  }
-
-  str_val = brightness.getValue();
-  if (str_val) {
-    int v = atoi(str_val);
-    if (v>0 && v<=MAX_BRIGHTNES) {
-      if (clock_brightness != v) {
-        clock_brightness = v;
-        EEPROM.write(eeprom_addr+1, clock_brightness);
-        EEPROM.commit();
-      }
-    }
-  }
-
-  str_val = n12.getValue();
-  if (str_val) {
-    int v = atoi(str_val);
-    if (v == 12) {
-      clock_12 = true;
-    } else {
-      clock_12 = false;
-    }
-    EEPROM.write(eeprom_addr+2, clock_12);
-    EEPROM.commit();
-  }
-
-  str_val = leading_zero.getValue();
-  if (str_val) {
-    if (strcmp(str_val, "y")==0) {
-      clock_leading_0 = true;
-    } else {
-      clock_leading_0 = false;
-    }
-    EEPROM.write(eeprom_addr+3, clock_leading_0);
-    EEPROM.commit();
-  }
-
-  str_val = ntp_server.getValue();
-  if (str_val) {
-    strncpy(ntpServerName, str_val, sizeof(ntpServerName)-1);
-    EEPROM.writeString(eeprom_addr+4, ntpServerName);
-    EEPROM.commit();
-  }
+  bool res = wm.autoConnect("NixieClock");
+  wm.stopWebPortal();
 
   return res;
 }
@@ -170,24 +99,55 @@ void setup()
   EEPROM.begin(100);
   Wire.begin();
 
-  // Read the EEPROM settings.
-  clock_tz = (signed char)EEPROM.read(eeprom_addr);
-  clock_brightness = EEPROM.read(eeprom_addr+1);
-  clock_12 = EEPROM.read(eeprom_addr+2);
-  clock_leading_0  = EEPROM.read(eeprom_addr+3);
+  read_eeprom_data();
 
-  log_printf("\ndoing setup: clock_tz=%d clock_brightnes=%d\n", clock_tz, clock_brightness);
-  EEPROM.readString(eeprom_addr+4, ntpServerName, sizeof(ntpServerName)-1);
+  log_printf("\ndoing setup: clock_tz=%d clock_brightness=%d\n", clock_tz, clock_brightness);
+
 
   get_time_from_rtc();
 
   if (initialize_network()) {
     setSyncProvider(getNtpTime);
-    setSyncInterval(3000);
+    setSyncInterval(NTP_UPDATE_INTERVAL);
+
+    IPAddress myIP = WiFi.localIP();
+    log_printf("AP IP address: %s\n", myIP.toString());
+
+    // start server portal
+    ui.attachBuild(build);
+    ui.attach(action);
+    ui.start();
   }
 }
 
-// DS-2300 segment control bits.
+void read_eeprom_data()
+{
+  // Read the EEPROM settings.
+  clock_tz = (signed char)EEPROM.read(eeprom_addr);
+  clock_brightness = EEPROM.read(eeprom_addr+1);
+  clock_12 = EEPROM.read(eeprom_addr+2);
+  clock_leading_0 = EEPROM.read(eeprom_addr+3);
+  clock_bar_mode = EEPROM.read(eeprom_addr+4);
+  clock_use_ntp = EEPROM.read(eeprom_addr+5);
+  clock_use_rtc = EEPROM.read(eeprom_addr+6);
+  EEPROM.readString(eeprom_addr+7, ntpServerName, sizeof(ntpServerName)-1);
+  EEPROM.commit();
+}
+
+void write_eeprom_data()
+{
+  EEPROM.write(eeprom_addr, clock_tz);
+  EEPROM.write(eeprom_addr+1, clock_brightness);
+  EEPROM.write(eeprom_addr+2, clock_12);
+  EEPROM.write(eeprom_addr+3, clock_leading_0);
+  EEPROM.write(eeprom_addr+4, clock_bar_mode);
+  EEPROM.write(eeprom_addr+5, clock_use_ntp);
+  EEPROM.write(eeprom_addr+6, clock_use_rtc);
+  EEPROM.writeString(eeprom_addr+7, ntpServerName);
+  EEPROM.commit();
+}
+
+// DA-2300 segment control bits.
 //
 //   64
 // 2   128
@@ -204,12 +164,12 @@ static const int digits[] = {
   2|4|8|16|32,     // 6
   16|64|128,       // 7
   255<<1,          // 8
-  2|16|32|64|128  // 9
+  2|16|32|64|128   // 9
 };
 
 // Display is 6 decimal digits.
 // If digit value is not 0-9 display blank value
-void show_disply(int *display)
+void show_disply(int *display, int *dots)
 {
   int i, j;
 
@@ -217,21 +177,18 @@ void show_disply(int *display)
   for (int i=0; i<6; i++) {
     int d = display[i];
     if (d>=0 && d<=9) {
-      // Translate digit to the segmnet bits
+      // Translate digit to the segment bits
       d = digits[d];
     } else {
       // Blank display
       d = 0;
     }
+    if (dots[i]) {
+      // Set the bar bit.
+      d |= 1;
+    }
 
-    // Set the bar bit.
-    digitalWrite(SCK_PIN, LOW);
-    digitalWrite(ONE_PIN, LOW);
-    digitalWrite(SER_PIN, HIGH);
-    digitalWrite(SCK_PIN, HIGH);
-    digitalWrite(ONE_PIN, HIGH);
-
-    for (j=1; j<8; j++) {
+    for (j=0; j<8; j++) {
       digitalWrite(SCK_PIN, LOW);
       digitalWrite(ONE_PIN, LOW);
       digitalWrite(SER_PIN, d&(1<<j) ? HIGH: LOW);
@@ -252,6 +209,18 @@ void loop()
   gettimeofday(&tv, NULL);
   struct tm *tm = localtime(&tv.tv_sec);
 
+  {
+    static long s;\
+    if (s!=tv.tv_sec) {
+      s = tv.tv_sec;
+      if (s%100 == 0) {
+        log_printf("LOOP %d:%02d:%02d\n", tm->tm_hour, tm->tm_min, tm->tm_sec);
+      }
+    }
+  }
+
+  ui.tick();
+
   // By default show current time
   int h = tm->tm_hour;
   if (clock_12 && h > 12) {
@@ -271,9 +240,31 @@ void loop()
   clock_display[4] = s/10;
   clock_display[5] = s%10;
 
-  show_disply(clock_display);
+  // Display separator bars 
+  int clock_dots[6] = {0, 0, 0, 0, 0, 0};
+
+  switch (clock_bar_mode) {
+    case 0:
+      // No dots to show
+      break;
+    case 1:
+      clock_dots[2] = tv.tv_usec >= 500000; 
+      clock_dots[4] = !clock_dots[2];
+      break;
+    case 2:
+      clock_dots[2] = tv.tv_usec >= 500000; 
+      clock_dots[4] = clock_dots[2];
+      break;
+    case 3:
+      clock_dots[2] = clock_dots[4] = 1;
+      break;
+  }
+
+  show_disply(clock_display, clock_dots);
+
   delay(BLINK_TIME);
-  if (clock_brightness < MAX_BRIGHTNES) {
+
+  if (clock_brightness < MAX_BRIGHTNESS) {
     unsigned int r = rand();
     // randomly select digits to blank.
     for (int i=0; i<6; i++) {
@@ -282,9 +273,9 @@ void loop()
       }
     }
     r >>= 6;
-    // Display dimed screen for short time.
-    show_disply(clock_display);
-    delay( (r%(MAX_BRIGHTNES-clock_brightness))*BLINK_TIME/MAX_BRIGHTNES );
+    // Display dimmed screen for short time.
+    show_disply(clock_display, clock_dots);
+    delay( (r%(MAX_BRIGHTNESS-clock_brightness))*BLINK_TIME/MAX_BRIGHTNESS );
   }
 }
 
@@ -351,4 +342,122 @@ void sendNTPpacket(IPAddress &address)
   Udp.beginPacket(address, 123); //NTP requests are to port 123
   Udp.write(packetBuffer, NTP_PACKET_SIZE);
   Udp.endPacket();
+}
+
+// Create a configuration form
+void build()
+{
+  log_printf("BUILD\n");
+
+  GP.BUILD_BEGIN();
+  GP.PAGE_TITLE("Nixie clock");
+
+  GP.THEME(GP_DARK);
+  GP.FORM_BEGIN("/update");
+
+  GP_MAKE_BLOCK_TAB(
+    "Clock config",
+    GP_MAKE_BOX(GP.LABEL("TimeZone shift:"); GP.NUMBER("clock_tz", "", clock_tz););
+    GP_MAKE_BOX(GP.LABEL("Brightness:"); GP.SLIDER("clock_brightness", clock_brightness, 1, MAX_BRIGHTNESS););
+    GP_MAKE_BOX(GP.LABEL("12/24 mode"); GP.SWITCH("clock_12", clock_12 ? true: false););
+    GP_MAKE_BOX(GP.LABEL("Leading 0"); GP.SWITCH("clock_leading_0", clock_leading_0 ? true: false, 0););
+    GP_MAKE_BOX(GP.LABEL("Bar mode"); GP.SELECT("clock_bar_mode", "Always on, Sync, Async, Always off", clock_bar_mode););
+    GP_MAKE_BOX(GP.LABEL("Use NTP"); GP.SWITCH("clock_use_ntp", clock_use_ntp ? true: false););
+    GP_MAKE_BOX(GP.LABEL("Use RTC"); GP.SWITCH("clock_use_rtc", clock_use_rtc ? true: false, 0););
+    GP_MAKE_BOX(GP.LABEL("NTP Server name: "); GP.TEXT("clock_ntp_server", "local NTP server if you have", ntpServerName, "", sizeof(ntpServerName)-1););
+  );
+  GP.SUBMIT("UPDATE");
+
+  GP.FORM_END();
+
+  GP.FORM_BEGIN("/settime");
+ 
+  struct timeval td;
+  gettimeofday(&td, NULL);
+  GPtime gptime (td.tv_sec);
+
+  GP_MAKE_BLOCK_TAB(
+    "Time",
+    GP_MAKE_BOX(GP.LABEL("Time :"); GP.TIME("time", gptime););
+  );
+  GP.SUBMIT("SET TIME");
+
+  GP.FORM_END();
+
+  GP.BUILD_END();
+}
+
+void action(GyverPortal& p)
+{
+  log_printf("ACTION\n");
+
+  if (p.form("/update")) {
+    int n;
+
+    log_printf("ACTION update\n");
+
+    // Reed the new values, and check them for sanity.
+    n = ui.getInt("clock_tz");
+    if (n>=-12 && n<=12) {
+      clock_tz = n;
+    }
+
+    n = ui.getInt("clock_brightness");
+    if (n>=1 && n<=MAX_BRIGHTNESS) {
+      clock_brightness = n;
+    }
+
+    n = ui.getBool("clock_12");
+    if (n>=0 && n<=1) {
+      clock_12 = n;
+    }
+
+    n = ui.getBool("clock_leading_0");
+    if (n>=0 && n<=1) {
+      clock_leading_0 = n;
+    }
+
+    n = ui.getInt("clock_bar_mode");
+    if (n>=0 && n<4) {
+      clock_bar_mode = n;
+    }
+
+    n = ui.getBool("clock_use_ntp");
+    if (n>=0 && n<=1) {
+      clock_use_ntp = n;
+    }
+
+    n = ui.getBool("clock_use_rtc");
+    if (n>=0 && n<=1) {
+      clock_use_ntp = n;
+    }
+
+    String s = ui.getString("clock_ntp_server");
+    if (s && strcmp(s.c_str(), ntpServerName) != 0) {
+      strncpy(ntpServerName, s.c_str(), sizeof(ntpServerName)-1);
+    }
+
+    write_eeprom_data();
+  }
+
+  if (p.form("/settime")) {
+    GPtime gptime = ui.getTime("time");
+    log_printf("Action Settime: %d:%02d:%02d\n", gptime.hour, gptime.minute, gptime.second);
+
+    // Set time to RPC
+    myRTC.setSecond(gptime.hour);
+    myRTC.setMinute(gptime.minute);
+    myRTC.setHour(gptime.second);
+
+    struct tm tm;
+    tm.tm_hour = gptime.hour;
+    tm.tm_min = gptime.minute;
+    tm.tm_sec = gptime.second;
+
+    struct timeval tv;
+    tv.tv_sec = mktime(&tm);
+    tv.tv_usec = 0;
+    // Set current time
+    settimeofday(&tv, NULL);
+  }
 }
